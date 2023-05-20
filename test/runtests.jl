88b758274249716414c55406bf8a1bc3acf7fd0e
Paul Tiede <ptiede91@gmail.com>
2023-05-20 15:15:54 -0400

    update test

--- test/runtests.jl
+++ test/runtests.jl
@@ -202,9 +202,9 @@ using Test
     @testset "GMRF" begin
         @testset "Tall" begin
             mimg = rand(10, 8)
-            d1 = GaussMarkovRF(mimg, 1.0, 2.0)
+            d1 = GaussMarkovRF(mimg, 3.0, 2.0)
             c = GMRFCache(mimg)
-            d2 = GaussMarkovRF(mimg, 1.0, 2.0, c)
+            d2 = GaussMarkovRF(mimg, 3.0, 2.0, c)
 
             x = rand(d1)
             @test logpdf(d1, x) ≈ logpdf(d2, x)
@@ -220,9 +220,9 @@ using Test
 
         @testset "Wide" begin
             mimg = rand(8, 10)
-            d1 = GaussMarkovRF(mimg, 1.0, 2.0)
+            d1 = GaussMarkovRF(mimg, 3.0, 2.0)
             c = GMRFCache(mimg)
-            d2 = GaussMarkovRF(mimg, 1.0, 2.0, c)
+            d2 = GaussMarkovRF(mimg, 3.0, 2.0, c)
 
             x = rand(d1)
             @test logpdf(d1, x) ≈ logpdf(d2, x)
@@ -237,10 +237,10 @@ using Test
         end
 
         @testset "Equal" begin
-            mimg = rand(8, 10)
-            d1 = GaussMarkovRF(mimg, 1.0, 2.0)
+            mimg = rand(10, 10)
+            d1 = GaussMarkovRF(mimg, 3.0, 2.0)
             c = GMRFCache(mimg)
-            d2 = GaussMarkovRF(mimg, 1.0, 2.0, c)
+            d2 = GaussMarkovRF(mimg, 3.0, 2.0, c)
 
             x = rand(d1)
             @test logpdf(d1, x) ≈ logpdf(d2, x)
